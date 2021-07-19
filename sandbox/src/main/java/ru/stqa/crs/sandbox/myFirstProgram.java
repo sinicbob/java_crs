115f76fff8d015d8d0b851610188402f7e96333d
sinicbob <sinicbob@gmail.com>
2021-07-19 20:07:43 +0000

    Изменены настройки форматирования

--- sandbox/src/main/java/ru/stqa/crs/sandbox/myFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/crs/sandbox/myFirstProgram.java
@@ -1,7 +1,7 @@
 package ru.stqa.crs.sandbox;
 
 public class myFirstProgram {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Hello world");
     }
 }
