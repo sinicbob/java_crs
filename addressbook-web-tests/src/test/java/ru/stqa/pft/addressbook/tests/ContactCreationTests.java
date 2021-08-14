61be8a72afaf139aea5bd5714ea6d9a2948af499
sinicbob <sinicbob@gmail.com>
2021-08-14 15:12:08 +0000

    Лекция № 6.8

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -22,16 +22,17 @@ public class ContactCreationTests extends TestBase{
 
   @DataProvider
   public Iterator<Object[]> validGroupsFromJson() throws IOException {
-    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));
-    String json = "";
-    String line = reader.readLine();
-    while (line != null){
-      json += line;
-      line = reader.readLine();
-    }
-    Gson gson = new Gson();
-    List<ContactData> groups = gson.fromJson(json, new TypeToken<List<ContactData>>() {}.getType());
-    return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+   try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")))){
+     String json = "";
+     String line = reader.readLine();
+     while (line != null){
+       json += line;
+       line = reader.readLine();
+     }
+     Gson gson = new Gson();
+     List<ContactData> groups = gson.fromJson(json, new TypeToken<List<ContactData>>() {}.getType());
+     return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+   }
   }
 
   @Test(dataProvider = "validGroupsFromJson")
