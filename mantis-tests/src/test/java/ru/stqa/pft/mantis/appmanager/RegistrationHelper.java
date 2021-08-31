82ee139ea002c634b8a179934964e65a10af9e5e
sinicbob <sinicbob@gmail.com>
2021-08-31 20:10:04 +0000

    лекция 8.6

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
@@ -1,18 +1,26 @@
 package ru.stqa.pft.mantis.appmanager;
 
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
 
-public class RegistrationHelper {
-  private final ApplicationManager app;
-  private WebDriver wd;
+public class RegistrationHelper extends HelperBase {
 
   public RegistrationHelper(ApplicationManager app) {
-    this.app = app;
+    super(app);
     wd = app.getDriver();
   }
 
   public void start(String username, String email) {
     wd.get(app.getProperty("web.baseUrl")+ "/signup_page.php");
+    type(By.name("username"),username);
+    type(By.name("email"),email);
+    click(By.cssSelector("input[class=\"width-40 pull-right btn btn-success btn-inverse bigger-110\"]"));
+  }
 
+  public void finish(String confirmationLink,String user, String password) {
+    wd.get(confirmationLink);
+    type(By.name("realname"),user);
+    type(By.name("password"),password);
+    type(By.name("password_confirm"),password);
+    click(By.cssSelector("button[class=\"width-100 width-40 pull-right btn btn-success btn-inverse bigger-110\"]"));
   }
 }
---
+++ mantis-tests/src/test/resources/config_inc.php
@@ -0,0 +1,14 @@
+<?php
+$g_hostname = '127.0.0.1';
+$g_db_type = 'mysqli';
+$g_database_name = 'bugtracker';
+$g_db_username = 'root';
+$g_db_password = '';
+$g_signup_use_captcha = OFF;
+
+$g_crypto_master_salt = '7T1qzG/fnjAR0+SZY0L+cQauUHJjwwfdHbhXmtp8Nt8=';
+
+$g_phpMailer_method  = PHPMAILER_METHOD_SMTP;
+$g_smtp_host = '127.0.0.1';
+
+
