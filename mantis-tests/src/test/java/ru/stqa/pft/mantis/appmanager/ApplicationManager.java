5cf6c37d9f5f1dc4b5a45b1d7ce57ea6f97182e4
sinicbob <sinicbob@gmail.com>
2021-09-08 18:08:45 +0000

    Лекция №9.3

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -23,6 +23,7 @@ public class ApplicationManager {
   private MailHelper mailHelper;
   private RefreshPasswordHelper refreshHelper;
   private DbHelper dbHelper;
+  private SoapHelper soapHelper;
 
   public ApplicationManager(String browser) {
     this.browser = browser;
@@ -96,6 +97,13 @@ public class ApplicationManager {
       dbHelper = new DbHelper(this);
     }
     return dbHelper;
+  }
+
+  public SoapHelper soap(){
+    if(soapHelper == null){
+      soapHelper = new SoapHelper(this);
+    }
+    return soapHelper;
   }
 
 }
