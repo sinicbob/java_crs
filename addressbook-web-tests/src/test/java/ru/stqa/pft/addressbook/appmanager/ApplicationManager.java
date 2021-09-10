299e4443adf070e1143c51f79f201e9d1aa1305d
sinicbob <sinicbob@gmail.com>
2021-09-10 14:46:24 +0000

    fix platform windows

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -50,7 +50,7 @@ public class ApplicationManager {
     }else {
       DesiredCapabilities capabilities = new DesiredCapabilities();
       capabilities.setBrowserName(browser);
-      capabilities.setPlatform(Platform.fromString(System.getProperty("platform","win10")));
+      capabilities.setPlatform(Platform.fromString(System.getProperty("platform","windows")));
       wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")),capabilities);
     }
 
